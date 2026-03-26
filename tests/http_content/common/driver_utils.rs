1736b66d5affa9aaedc50dbd8382b627d0d5c9d4
wkkasztan <wiktor.kasjaniuk@proton.me>
2026-03-26 20:52:09 +0100

    Add header stripping tests

---
+++ tests/http_content/common/driver_utils.rs
@@ -0,0 +1,31 @@
+use alternator_driver::client::Waiters;
+use std::time::Duration;
+
+pub async fn delete_table_cleanup(client: &alternator_driver::Client, table_name: &str) {
+    // try to delete
+    let mut result = client.delete_table().table_name(table_name).send().await;
+
+    // wait if resource is not yet ready
+    if let Err(ref e) = result
+        && e.as_service_error()
+            .is_some_and(|s| s.is_resource_in_use_exception())
+    {
+        client
+            .wait_until_table_exists()
+            .table_name(table_name)
+            .wait(Duration::from_secs(5))
+            .await
+            .unwrap();
+
+        result = client.delete_table().table_name(table_name).send().await;
+    }
+
+    // final check
+    if let Err(e) = result
+        && !e
+            .as_service_error()
+            .is_some_and(|s| s.is_resource_not_found_exception())
+    {
+        panic!("Cleanup failed: {e:?}");
+    }
+}
--- tests/http_content/mod.rs
+++ tests/http_content/mod.rs
@@ -4,4 +4,8 @@ mod proxy;
 #[path = "common/http_test.rs"]
 mod http_test;
 
+#[path = "common/driver_utils.rs"]
+mod driver_utils;
+
 pub mod correct_line;
+pub mod header_whitelist;
